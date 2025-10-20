6e28dd22b833ff60cbbefd237cae94b499ce87b2
hran07 <gibrankun07@gmail.com>
2025-10-20 11:02:35 +0700

    Ketiga

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,6 @@ public class Main {
         int a = sc.nextInt();
 
         System.out.println("output: " + a);
+        System.out.println("test");
     }
 }
-
-/*
-# Codelab 1 & 2 - Java Input Output
-
-## Deskripsi
-Project ini dibuat untuk latihan dasar penggunaan Git di IntelliJ IDEA (Codelab 1) dan penulisan dokumentasi proyek menggunakan README.md (Codelab 2).
-
-## Cara Menjalankan
-1. Jalankan file `Main.java`
-2. Program akan meminta input angka:
-
-3. Masukkan angka berapa pun, lalu tekan Enter
-4. Output akan muncul seperti:
-
-## Contoh Hasil Program
-input: 7
-output: 7
-
-
-
- */
